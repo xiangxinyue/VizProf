adc87fa81f7679e4c2aaa27bc1488f5513d72f1d
Minci Zhou <zmc1996@hotmail.com>
2019-01-13 13:40:55 -0700

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,12 +23,39 @@ export class MapContainer extends Component {
 		this.setState({
 		selectedPlace: props,
 		activeMarker: marker,
+		showingInfoWindow: true,
+		
+    });
+
+	showPopUpOfMarker = (props, marker, e) =>
+		this.setState({
+		selectedPlace: props,
+		activeMarker: marker, 
 		showingInfoWindow: true
     });
-    
+	hidePopUpOfMarker = (props, marker, e) =>
+		this.setState({
+		selectedPlace: props,
+		showingInfoWindow: false
+    });    
+	
+	onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
+
+  
 	render() {
-         var highlightedIcon = {
-        url:  'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|FFFF24|40|_|%E2%80%A2', // url
+         var goodIcon = {
+        url:  'https://image.flaticon.com/icons/svg/290/290633.svg', // url
+        scaledSize: new this.props.google.maps.Size(20, 30), // scaled size
+    };
+	    var badIcon = {
+        url:  'https://image.flaticon.com/icons/svg/361/361583.svg', // url
         scaledSize: new this.props.google.maps.Size(20, 30), // scaled size
     };
 	return (
@@ -44,33 +71,74 @@ export class MapContainer extends Component {
 		<Marker 
 		    onClick={this.onMarkerClick}
 	    	title={'Administration Building'}
-			name={'University of Alberta'} />		
+			name={'University of Alberta'} 			
+			/>		
 		<Marker
 			onClick={this.onMarkerClick}
 			title={'Computing Science'}
 			name={'3.44'}
-			position={{lat: 53.526805, lng: -113.526671}} />
+			position={{lat: 53.526805, lng: -113.526671}} 
+			icon={badIcon}
+			/>
 		<Marker
 			onClick={this.onMarkerClick}
 			title={'Chemistry'}
 			name={'3.64'}
 			position={{lat: 53.527470, lng: -113.524521}} 
-			icon={highlightedIcon}
  
-			
-			
+			icon={badIcon}
 			/>	
 		<Marker
 			onClick={this.onMarkerClick}
 			title={'Mechanical Engineering'}
 			name={'3.66'}
-			position={{lat: 53.527736, lng: -113.527784	}} />	
+			position={{lat: 53.527736, lng: -113.527784	}}
+			icon={badIcon}
+			/>	
+		<Marker
+			onClick={this.onMarkerClick}
+			title={'Art'}
+			name={'4.13'}
+			position={{lat: 53.524387, lng:  -113.519970	}}
+			icon={goodIcon}
+			/>	
+			
+		<Marker
+			onClick={this.onMarkerClick}
+			title={'Law'}
+			name={'3.91'}
+			position={{lat: 53.524405, lng: -113.518570	}}
+			icon={badIcon}
+			/>		
+		<Marker
+			onClick={this.onMarkerClick}
+			title={'Economics'}
+			name={'3.45'}
+			position={{lat: 53.527749, lng: -113.521533}}
+			icon={badIcon}
+			/>		
+		<Marker
+			onClick={this.onMarkerClick}
+			title={'Business'}
+			name={'3.66'}
+			position={{lat: 53.527371, lng: -113.521721}}
+			icon={badIcon}
+			/>	
 
+		<Marker
+			onClick={this.onMarkerClick}
+			title={'Statistics'}
+			name={'3.18'}
+			position={{lat: 53.526622,  lng: -113.524676}}
+			icon={badIcon}
+			/>	
+			
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}>
             <div>
-              <h1>{this.state.selectedPlace.name}</h1>
+              <h1>{this.state.selectedPlace.title}</h1>
+			  <h2>{this.state.selectedPlace.name}</h2>
             </div>
         </InfoWindow>
 		</Map>
@@ -85,3 +153,4 @@ export default GoogleApiWrapper({
 //export class MapContainer extends React.Component {}
  
 //export default GoogleApiWrapper({apiKey: ('AIzaSyCA32YtSecGralPoFapbqOSR1pY58EXwRA')})(MapContainer);
+
